e81a7cb32387ff209ac00e7a042c1a0d298ea89c
Bianca-Ana-Maria <ababeibianca16@gmail.com>
2021-04-06 14:25:40 +0300

    python

--- dd.py
+++
@@ -1,6 +0,0 @@
- hello-world
-
-Hello!
-
-My name is Ababei Bianca Ana Maria and I am 22 years old.
-I want to learn git and get familiar with various IT programs.
---
+++ project.py
@@ -0,0 +1,9 @@
+x = 7
+y = 8
+z = 2
+result1 = x == y
+result2 = y > x
+result3 = z < x + 2
+result4= result1 and result2
+print(result4)
+print(not (True and False or True))
