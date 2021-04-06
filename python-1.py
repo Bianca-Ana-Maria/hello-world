47ec3588c75b0022f5f3a41ba69e27ad3e1fe0de
Bianca-Ana-Maria <81956136+Bianca-Ana-Maria@users.noreply.github.com>
2021-04-06 16:04:28 +0300

    Add Output

--- python-1.py
+++ python-1.py
@@ -1,17 +1,15 @@
-#not
-#and(F)
-#or(T)
-x = 7
+#1
+x = 7                                                          Output:
 y = 8
 z = 0
 result1 = x == y
 result2 = y > x
 result3 = z < x + 2
-result4= result1 and result2
-print(result4)
-print(not (True and False or True))
+result4= result1 and result2                                     
+print(result4)                                                    = False
+print(not (True and False or True))                               = False
 
-
+#2
 x = input('Name: ')
 if x == 'Bianca':
     print('You are great!')
@@ -23,39 +21,25 @@ else:
     print('You are ok')
 
 
-#len(lungime)
-#.append(adauga la var x);extend
-#pop(elimina; 0-primul element,1-...)
+#3
 x = [4, True, 'hi']
 y = 'hello'
-print(len(x), len(y))
-
-x.append('hello')
-print(x)
-
-print(x.pop(0))
-print(x)
-
+print(len(x), len(y))                                             = 3, 5
+x.append('hello')       
+print(x)                                                          = [4, True, 'hi' , 'hello']
+print(x.pop(0))                                                  
+print(x)                                                          = [True, 'hi' , 'hello']
 x[2] = 'car'
-print(x)
+print(x)                                                          = [True, 'hi' , 'car']
 
-#for loops
-#(start,stop,step)
-
-for i in range(10, -1, -1):
+#4
+for i in range(10, -1, -1):                                       = 10  9  8  7  6  5  4  3  2  1  0
     print(i)
 
 x = [3,6,8,45,32,5]
 for i, element in enumerate(x):
-    print(i, element)
-
-#while loops
-
-x = [3,4,42,3,2,4]
-
-i = 0
-while True:
-    print('run')
+    print(i, element)                                             = 0 3        1 6       2 8      3 45      4 32      5 5
+                                                             
 
 
 
